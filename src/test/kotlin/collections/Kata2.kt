995326e6f72fdf3e3037fdff4422114b916e834f
Norlander, Brian <brian.norlander@rakuten.com>
2020-06-15 22:49:41 +0900

    sols to kata2

--- src/test/kotlin/collections/Kata2.kt
+++ src/test/kotlin/collections/Kata2.kt
@@ -4,12 +4,6 @@ import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 
-/*
-    Operators and Filter
-    +, -
-    filter{ }
- */
-
 class Kata2 {
 
     @Nested
@@ -18,21 +12,21 @@ class Kata2 {
         fun `Add two lists together`() {
             val nums1 = listOf(1, 2, 3)
             val nums2 = listOf(4, 5, 6)
-            val nums3 = TODO("In kotlin, lists are read-only, which means adding two lists together creates a new list")
+            val nums3 = nums1 + nums2
 
             assertEquals(listOf(1, 2, 3, 4, 5, 6), nums3)
         }
 
         @Test
         fun `Remove an item from a list`() {
-            val nums = listOf(1, 2, 3)
+            val nums = listOf(1, 2, 3) - 3
 
             assertEquals(listOf(1, 2), nums)
         }
 
         @Test
         fun `Add another entry to a map`() {
-            val map = mapOf(1 to "One", 2 to "Two")
+            val map = mapOf(1 to "One", 2 to "Two") + Pair(3, "Three")
 
             assertEquals(mapOf(1 to "One", 2 to "Two", 3 to "Three"), map)
         }
@@ -40,6 +34,7 @@ class Kata2 {
         @Test
         fun `Add an element to a mutable list`() {
             val nums = mutableListOf(1, 2, 3)
+            nums += 4
 
             assertEquals(mutableListOf(1, 2, 3, 4), nums)
         }
@@ -50,6 +45,7 @@ class Kata2 {
         @Test
         fun `Filter numbers less than 5`() {
             val nums = listOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+                .filter { it < 5 }
 
             assertEquals(listOf(1, 2, 3, 4), nums)
         }
@@ -57,6 +53,7 @@ class Kata2 {
         @Test
         fun `Filter even numbers only`() {
             val nums = listOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+                .filter { it % 2 == 0 }
 
             assertEquals(listOf(2, 4, 6, 8, 10), nums)
         }
@@ -64,17 +61,34 @@ class Kata2 {
         @Test
         fun `Filter names with a length greater than 4`() {
             val names = listOf("Steve", "Elon", "Larry", "Jeff", "Mark", "Tim", "Sundar")
+                .filter { it.length > 4 }
 
             assertEquals(listOf("Steve", "Larry", "Sundar"), names)
         }
 
         @Test
-        fun `Filter a map by the keys`() {
+        fun `Filter a map before the year 1960`() {
             val names = mapOf(
                 1971 to "Elon Musk",
                 1955 to "Steve Jobs",
                 1944 to "Larry Ellison"
+            ).filterKeys { it < 1960 }
+
+            assertEquals(
+                mapOf(
+                    1955 to "Steve Jobs",
+                    1944 to "Larry Ellison"
+                ), names
             )
+        }
+
+        @Test
+        fun `Filter Steve Jobs out of a map`() {
+            val names = mapOf(
+                1971 to "Elon Musk",
+                1955 to "Steve Jobs",
+                1944 to "Larry Ellison"
+            ).filterValues { it != "Steve Jobs" }
 
             assertEquals(
                 mapOf(
@@ -85,24 +99,11 @@ class Kata2 {
         }
 
         @Test
-        fun `Filter a map by the values`() {
-            val names = mapOf(
-                1971 to "Elon Musk",
-                1955 to "Steve Jobs",
-                1944 to "Larry Ellison"
-            )
-
-            assertEquals(
-                mapOf(
-                    1971 to "Elon Musk",
-                    1944 to "Larry Ellison"
-                ), names
-            )
-        }
-
-        @Test
-        fun `Filter by index`() {
+        fun `Filter out indexes 2 and 3`() {
             val nums = listOf(1, 2, 3, 4, 5)
+                .filterIndexed { index, i ->
+                    index != 2 && index != 3
+                }
 
             assertEquals(listOf(1, 2, 5), nums)
         }

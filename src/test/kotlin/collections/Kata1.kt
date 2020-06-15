699311b3b57b07f0cae7fc5af63c6d1210e13bfc
Norlander, Brian <brian.norlander@rakuten.com>
2020-06-15 22:37:05 +0900

    reformat, refactor kata1

--- src/test/kotlin/collections/Kata1.kt
+++ src/test/kotlin/collections/Kata1.kt
@@ -3,10 +3,6 @@ package collections
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
-/*
-    Create arrays, lists, sets, and maps
- */
-
 class Kata1 {
     @Test
     fun `Create an empty list of Strings`() {
@@ -20,13 +16,6 @@ class Kata1 {
         val emptySetOfString = TODO()
 
         assertEquals(emptySetOfStringSolution, emptySetOfString)
-    }
-
-    @Test
-    fun `Create an int array with the elements 1, 2, 3`() {
-        val numsOneTwoThree = TODO()
-
-        assertEquals(numsOneTwoThreeSolution, numsOneTwoThree)
     }
 
     @Test
